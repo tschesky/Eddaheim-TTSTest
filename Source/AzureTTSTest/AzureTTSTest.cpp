96886dffbdc2037791c8e3b95fddd0ca9979c065
Michal <Michal@Eddaheim.com>
2021-12-20 11:41:55 +0100

    Init

---
+++ Source/AzureTTSTest/AzureTTSTest.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AzureTTSTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AzureTTSTest, "AzureTTSTest" );
---
+++ Source/AzureTTSTest/AzureTTSTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AzureTTSTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AZURETTSTEST_API AAzureTTSTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
